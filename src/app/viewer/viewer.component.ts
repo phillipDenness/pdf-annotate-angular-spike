e2cf66d5ef613665ed6a9352c6e0b63963c5817c
Phillip Denness <dennessphillip@gmail.com>
2018-09-07 15:00:20 +0100

    Demo

--- src/app/viewer/viewer.component.ts
+++ src/app/viewer/viewer.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, AfterViewInit, ElementRef, Inject} from '@angular/core';
 import { DOCUMENT } from '@angular/common';
+import { PDFAnnotations, PDFAnnotationData } from 'pdfjs-dist';
+import * as PDFANNO from 'assets/pdf-annotate';
+
 
 @Component({
   selector: 'app-viewer',
@@ -7,49 +10,15 @@ import { DOCUMENT } from '@angular/common';
   styleUrls: ['./viewer.component.scss']
 })
 export class ViewerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  data: PDFAnnotationData;
+  
+  constructor(pDFAnnotations:PDFAnnotations) { 
+    this.data = pDFAnnotations.getData();
   }
 
-  // ngAfterViewInit()
-  // {
-  //   const s = this.document.createElement('script');
-  //   s.type = 'text/javascript';
-  //   s.src = '//mozilla.github.io/pdf.js/build/pdf.js';
-  //   const g = this.document.createElement('script');
-  //   g.type = 'text/javascript';
-  //   g.src = '/assets/pdf-annotate.js';
-
-  //   const t = this.document.createElement('script');
-  //   t.type = 'text/javascript';
-  //   t.src = '/assets/demo.js';
-
-
-  //   const __this = this; //to store the current instance to call 
-  //                        //afterScriptAdded function on onload event of 
-  //                        //script.
-  //   s.onload = function () { __this.afterScriptAdded(); };
-  //   // this.elementRef.nativeElement.appendChild(s);
-  //   // this.elementRef.nativeElement.appendChild(g);
-  //    this.elementRef.nativeElement.appendChild(t);
-  // }
-
-  // afterScriptAdded() {
-  //   const RENDER_OPTIONS = {
-  //     documentId: '/assets/example.pdf',
-  //     pdfDocument: null,
-  //     scale: 1,
-  //     rotate: 0
-  //   };
-  //   if (typeof (window['functionFromExternalScript']) === 'function') {
-  //     window['functionFromExternalScript'](RENDER_OPTIONS);
-  //   }
-  // }
-//   <script src="src/assets/pdf.js"></script>
-// <script src="src/assets/pdf_viewer.js"></script>
-// <script src="src/assets/index.js"></script>
+  ngOnInit() {
+    
+  }
 }
 
 
